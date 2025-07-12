6b3367968046c7bc104ee97af1f310f3611b6051
Rohan Pagare <rohanpagare7599@gmail.com>
2025-07-12 13:13:48 +0530

    remove console

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,6 @@ import mongoose from 'mongoose';
 import {app} from "./app";
 
 const startDbConnection = async () => {
-    console.log('starting');
     if(!process.env.JWT_KEY) {
         throw  new Error("JWT_KEY must be defined!")
     }
